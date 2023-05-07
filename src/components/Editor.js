4bcea4c3a9661cea87658c81dc096b920c3cb83c
imsachin010 <007mishrasachinmishra@gmail.com>
2023-05-07 20:57:09 +0530

    local storage saving

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,6 +5,10 @@ import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import {Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faC, faCompressAlt, faE, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+
+
 export default function Editor(props) {
     const {
         value,
@@ -22,7 +26,14 @@ export default function Editor(props) {
       <div className={'editor-container ${open ? '' : 'collapsed'}'}>
         <div className='editor-title'>
             {displayName}
-            <button onClick={() =>setOpen(prevOpen => !prevOpen)}>o/c</button>
+            <button
+              type='button'
+              className='expand-collapsed-btn'
+              onClick={() =>setOpen(prevOpen => !prevOpen)}
+            >
+              <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt}/>
+            
+            </button>
         </div>
         <ControlledEditor/>
         onBeforeChange={handleChange}
---
+++ src/hooks/uselocalstorage.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+
+const PREFIX = 'dev-pen'
+
+export default function uselocalstorage(key, initialValue) 
+{
+    const prefixedkey = PREFIX+ key
+    
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedkey)
+        if ( jsonValue != null) return JSON.parse(jsonValue)
+        if ( typeof initialValue === 'function') {
+            return initialValue()
+        } else {
+            return initialValue
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(prefixedkey, JSON.stringify(value))
+    },[prefixedkey, value])
+    
+    return (
+        <div>
+        
+        </div>
+    )
+}
